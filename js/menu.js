88fecb222b3712810f538808a851f7c94604146f
Darkcode2218 <133794430+DarkABCode@users.noreply.github.com>
2023-10-25 00:14:27 +0100

    Se agrega el proyecto

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+// const hamburguer = document.querySelector('.hamburger');
+// const menu = document.querySelector('.menu-navegacion');
+
+// // console.log(menu)
+// // console.log(hamburguer)
+
+
+// hamburguer.addEventListener('click', ()=>{
+//    menu.classList.toggle("spread")
+// })
+
+// window.addEventListener('click', e=>{
+//     if (menu.classList.contains('spread')&& e.target != menu && e.target != hamburguer){
+
+//         menu.classList.toggle("spread")
+//     }
+// })
+
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    document.querySelector('.nav-list').classList.toggle('active');
+});
